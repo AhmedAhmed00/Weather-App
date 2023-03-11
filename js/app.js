392c527ec945fc89601fcf8e8a3a0c1382e18a4f
ahmed <devahmedahmed@gmail.com>
2023-03-11 09:33:49 +0200

    delete variables and fix next days bug

--- js/app.js
+++ js/app.js
@@ -23,6 +23,8 @@ let days = [
   "Saturday",
 ];
 let searchInput = document.querySelector(".search-input");
+let today = document.querySelector(".item-today");
+let nextDay = document.querySelectorAll(".next-day");
 
 // get default data
 getWether();
@@ -42,48 +44,80 @@ async function getWether(name = "London") {
 
 // get Today data
 function getTodayData(temp) {
-  let today = document.querySelector(".item-today .the-day");
-  let todayDate = document.querySelector(".item-today .the-date");
-  let city = document.querySelector(".item-today .city");
-  let temprtureDeg = document.querySelector(".item-today .temperature-deg");
-  let temprtureStatus = document.querySelector(
-    ".item-today .temperature-status"
-  );
-  let tempIcon = document.querySelector(".item-today .temperature-icon");
-  let humadityDeg = document.querySelector(".item-today .humidity-deg");
-  let windSpeedDeg = document.querySelector(".item-today .wind-speed-deg");
-  let direction = document.querySelector(".item-today .direction-deg");
-  today.innerHTML = days[date.getDay()];
-  todayDate.innerHTML = `${date.getDate()} ${monthName[date.getMonth()]}`;
-  city.innerHTML = temp.location.name;
-  temprtureDeg.innerHTML = `${temp.forecast.forecastday[0].day.maxtemp_c}<sup>o</sup>C `;
-  temprtureStatus.innerHTML = temp.current.condition.text;
-  tempIcon.setAttribute("src", `https:${temp.current.condition.icon}`);
-  humadityDeg.innerHTML = `${temp.forecast.forecastday[0].day.avghumidity}%`;
-  windSpeedDeg.innerHTML = `${temp.current.wind_kph}%`;
-  direction.innerHTML = `${temp.current.wind_dir}`;
+  let html = `
+                <div class="head d-flex justify-content-between px-3">
+                  <p class="the-day pt-3">${days[date.getDay()]}</p>
+                  <p class="the-date pt-3">${date.getDate()} ${
+    monthName[date.getMonth()]
+  }</p>
+                </div>
+
+                <div class="content p-3 text-white">
+                  <div class="city fs-4 fw-bold my-3 px-3"> ${
+                    temp.location.name
+                  }</div>
+                  <div
+                    class="temperature d-flex justify-content-between align-items-center px-3"
+                  >
+                    <p class="temperature-deg fw-bold">${
+                      temp.forecast.forecastday[0].day.maxtemp_c
+                    }<sup>o</sup>C</p>
+                    <img class="temperature-icon" width="90" src="https:${
+                      temp.current.condition.icon
+                    }" />
+                  </div>
+                  <span class="temperature-status d-block my-4 px-3"
+                    >${temp.current.condition.text}</span
+                  >
+                  <div class="temperature-info d-flex px-3">
+                    <div class="humidity d-flex align-items-baseline me-3">
+                      <i class="fas fa-umbrella pe-1"></i>
+                      <p class="humidity-deg">${
+                        temp.forecast.forecastday[0].day.avghumidity
+                      }%</p>
+                    </div>
+                    <div class="wind-speed d-flex align-items-baseline me-3">
+                      <i class="fas fa-wind pe-1"></i>
+                      <p class="wind-speed-deg">${temp.current.wind_kph}%</p>
+                    </div>
+                    <div class="direction d-flex align-items-baseline me-3">
+                      <i class="far fa-compass pe-1"></i>
+                      <p class="direction-deg">${temp.current.wind_dir}</p>
+                    </div>
+                  </div>
+            </div>
+`;
+  today.innerHTML = html;
 }
 
 // get the next day data
 function getTheNextDay(temp) {
-  let theNext = document.querySelectorAll(".next-day");
-  let today = document.querySelectorAll(".next-day .the-day");
-  let maxDeg = document.querySelectorAll(".next-day .max-deg");
-  let minDeg = document.querySelectorAll(".next-day .min-deg");
-  let temprtureStatus = document.querySelectorAll(
-    ".next-day .temperature-status"
-  );
-  let tempIcon = document.querySelectorAll(".next-day .temperature-icon");
-  for (let i = 0; i < theNext.length; i++) {
-    today[i].innerHTML = days[date.getDay() + i + 1];
-    tempIcon[i].setAttribute(
-      "src",
-      `https:${temp.forecast.forecastday[i + 1].day.condition.icon}`
-    );
-    maxDeg[i].innerHTML = `${temp.forecast.forecastday[i + 1].day.maxtemp_c} `;
-    minDeg[i].innerHTML = `${temp.forecast.forecastday[i + 1].day.mintemp_c} `;
-    temprtureStatus[i].innerHTML =
-      temp.forecast.forecastday[i].day.condition.text;
+  for (let i = 0; i < nextDay.length; i++) {
+    let html = `
+       <div class="head">
+                  <p class="the-day pt-3">${days[checkNextDay() + i]}</p>
+                </div>
+                <div class="content p-4 text-white">
+                  <div class="next-day-icon px-3">
+                    <img class="temperature-icon" width="90"  src="https:${
+                      temp.forecast.forecastday[i + 1].day.condition.icon
+                    }"/>
+                  </div>
+                  <div class="temperature">
+                    <p class="max-deg fs-3">${
+                      temp.forecast.forecastday[i + 1].day.maxtemp_c
+                    }</p>
+                    <p class="min-deg mb-4">${
+                      temp.forecast.forecastday[i + 1].day.mintemp_c
+                    }</p>
+                  </div>
+                  <span
+                    class="temperature-status d-inline-block mb-4 px-3"
+                  >${temp.forecast.forecastday[i].day.condition.text}</span>
+                </div>
+
+    `;
+    nextDay[i].innerHTML = html;
   }
 }
 
@@ -92,5 +126,13 @@ function search() {
   getWether(searchInput.value);
   if (searchInput.value === "") {
     getWether();
+  }
+}
+
+function checkNextDay() {
+  if (date.getDay() === 6) {
+    return 0;
+  } else {
+    return date.getDay();
   }
 }
